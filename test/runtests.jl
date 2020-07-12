29ee6242e7ab0d17d93583c87854940d88ca4a1e
Liu, Haitao <htyeim@gmail.com>
2020-07-12 15:34:41 +0800

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ using Glob
 @testset "HTgnss.jl" begin
     # Write your own tests here.
     # fn = "/home/t/RD/GNSS/STEC/-1_00_01/2015/115/FINS00ALA_R_20151160000_01D_30S_MO.rnx"
+    # fn = "/home/t/RD/GNSS/STEC/-1_00_01/2015/115/NYA200SJM_R_20151140000_01D_30S_MO.rnx"
+
     # HTgnss.load_obs(fn)
 
 
