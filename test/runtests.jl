107abfc16269059ca642c24a7fe811c94208280c
Liu, Haitao <htyeim@gmail.com>
2020-07-12 08:45:52 +0800

    test template

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,10 @@ using Glob
 
 @testset "HTgnss.jl" begin
     # Write your own tests here.
+    # fn = "/home/t/RD/GNSS/STEC/-1_00_01/2015/115/FINS00ALA_R_20151160000_01D_30S_MO.rnx"
+    # HTgnss.load_obs(fn)
+
+
     include("read_obsnav.jl")
     test_pos()
 end
